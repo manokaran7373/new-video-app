b2183754b513e5351253801b4c20fd2c74b80797
manokaran7373 <manokarank1916@gmail.com>
2025-06-12 23:57:06 +0530

    updated

--- app.py
+++ app.py
@@ -4,9 +4,12 @@ import uuid
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
+# Store active rooms and their participants
 rooms = {}
+# Store user connections
+user_connections = {}
 
 @app.route('/')
 def index():
@@ -15,31 +18,109 @@ def index():
 @app.route('/create-meeting')
 def create_meeting():
     meeting_id = str(uuid.uuid4())
+    rooms[meeting_id] = []
     return {'meeting_id': meeting_id}
 
 @app.route('/join/<meeting_id>')
 def join_meeting(meeting_id):
     return render_template('meeting.html', meeting_id=meeting_id)
 
+@socketio.on('connect')
+def handle_connect():
+    user_connections[request.sid] = None
+
 @socketio.on('join')
 def on_join(data):
     username = data['username']
     room = data['room']
+    
     join_room(room)
+    user_connections[request.sid] = {'room': room, 'username': username}
+    
     if room not in rooms:
         rooms[room] = []
-    rooms[room].append({'username': username, 'sid': request.sid})
-    emit('user_joined', {'username': username, 'users': rooms[room]}, room=room)
+    
+    # Add new user to room
+    rooms[room].append({
+        'username': username,
+        'sid': request.sid
+    })
+    
+    # Notify everyone in the room about the new user
+    emit('user_joined', {
+        'username': username,
+        'users': rooms[room]
+    }, room=room)
+    
+    # Send existing peers to the new user
+    emit('existing_peers', {
+        'users': rooms[room],
+        'current_user': username
+    }, room=request.sid)
+
+@socketio.on('start_call')
+def start_call(data):
+    room = data['room']
+    target_sid = data['target_sid']
+    emit('call_user', {
+        'offer': data['offer'],
+        'caller': request.sid
+    }, room=target_sid)
+
+@socketio.on('accept_call')
+def accept_call(data):
+    caller_sid = data['caller']
+    emit('call_accepted', {
+        'answer': data['answer'],
+        'acceptor': request.sid
+    }, room=caller_sid)
+
+@socketio.on('ice_candidate')
+def handle_ice_candidate(data):
+    target_sid = data['target_sid']
+    emit('ice_candidate', {
+        'candidate': data['candidate'],
+        'sender': request.sid
+    }, room=target_sid)
 
 @socketio.on('leave')
 def on_leave(data):
     username = data['username']
     room = data['room']
-    leave_room(room)
-    rooms[room] = [user for user in rooms[room] if user['sid'] != request.sid]
-    emit('user_left', {'username': username, 'users': rooms[room]}, room=room)
+    
+    if room in rooms:
+        rooms[room] = [user for user in rooms[room] if user['sid'] != request.sid]
+        leave_room(room)
+        
+        emit('user_left', {
+            'username': username,
+            'sid': request.sid,
+            'users': rooms[room]
+        }, room=room)
+        
+        if len(rooms[room]) == 0:
+            del rooms[room]
 
-# Add this new route handler in your existing app.py
+@socketio.on('disconnect')
+def handle_disconnect():
+    if request.sid in user_connections:
+        user_data = user_connections[request.sid]
+        if user_data:
+            room = user_data['room']
+            username = user_data['username']
+            if room in rooms:
+                rooms[room] = [user for user in rooms[room] if user['sid'] != request.sid]
+                emit('user_left', {
+                    'username': username,
+                    'sid': request.sid,
+                    'users': rooms[room]
+                }, room=room)
+                
+                if len(rooms[room]) == 0:
+                    del rooms[room]
+        
+        del user_connections[request.sid]
+
 @socketio.on('chat_message')
 def handle_message(data):
     room = data['room']
@@ -48,6 +129,5 @@ def handle_message(data):
         'message': data['message']
     }, room=room)
 
-
 if __name__ == '__main__':
-    socketio.run(app, debug=True,allow_unsafe_werkzeug=True)
+    socketio.run(app, debug=True, allow_unsafe_werkzeug=True)
